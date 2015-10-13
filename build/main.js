86ac3fffa95b6fd3174c95a967002c5c290356cf
Timothy Castillo <tacastillo@usfca.edu>
2015-10-13 02:18:35 -0700

    Committing app

---
+++ build/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Timothy on 10/12/15.
+ */
+
+
+React.render(React.createElement(Board, {count: 50}),
+    document.getElementById('react-container'));
---
+++ js/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Timothy on 10/12/15.
+ */
+
+React.render(<Board count={50}/>,
+    document.getElementById('react-container'));
